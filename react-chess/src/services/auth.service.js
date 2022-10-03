6d0c8f796ce563fc1d4979a54a0790c91b9d44f8
vlad-serkov <lordr7638@gmail.com>
2022-10-04 00:56:18 +0300

    react

---
+++ react-chess/src/services/auth.service.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/";
+
+const register = (firstName, lastName, email, password) => {
+  return axios.post(API_URL + "api/v1/registration", {
+    firstName,
+    lastName,
+    email,
+    password,
+  });
+};
+
+const login = (username, password) => {
+  return axios
+    .post(API_URL + "login", {
+      username,
+      password,
+    })
+    .then((response) => {
+      console.log(response);
+      if (response.data.accessToken) {
+
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+      console.log(response.data);
+      return response.data;
+    });
+};
+
+const logout = () => {
+  localStorage.removeItem("user");
+};
+
+const getCurrentUser = () => {
+  return JSON.parse(localStorage.getItem("user"));
+};
+
+const AuthService = {
+  register,
+  login,
+  logout,
+  getCurrentUser,
+};
+
+export default AuthService;
